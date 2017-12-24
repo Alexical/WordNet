0e2b6843bf9819c7294a5aa95fe00afb17388aa0
Alex Lee <phparkle@gmail.com>
2017-12-24 21:40:47 +0800

    Implement Deque<E>

--- src/FastArrayDeque.java
+++ src/FastArrayDeque.java
@@ -1,10 +1,11 @@
-import java.util.AbstractCollection;
+import java.util.AbstractQueue;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class FastArrayDeque<E> extends AbstractCollection<E> {
+public class FastArrayDeque<E> extends AbstractQueue<E> implements Deque<E> {
 
     private static final int INIT_CAPACITY = 4;
 
@@ -25,14 +26,6 @@ public class FastArrayDeque<E> extends AbstractCollection<E> {
         reset(initialCapacity);
     }
 
-    public boolean isEmpty() {
-        return n == 0;
-    }
-
-    public int size() {
-        return n;
-    }
-
     public void reset() {
         reset(INIT_CAPACITY);
     }
@@ -43,22 +36,36 @@ public class FastArrayDeque<E> extends AbstractCollection<E> {
         j = initialCapacity;
     }
 
-    public void clear() {
-        n = 0;
-        if (a != null)
-            j = a.length;
-    }
-
-    public boolean add(E item) {
-        addLast(item);
-        return true;
-    }
-
+    @Override
     public void push(E item) {
         addFirst(item);
     }
 
-    public void addFirst(E item) {
+    @Override
+    public boolean add(E e) {
+        addLast(e);
+        return true;
+    }
+
+    @Override
+    public void addFirst(E e) {
+        offerFirst(e);
+    }
+
+    @Override
+    public void addLast(E e) {
+        offerLast(e);
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return offerFirst(e);
+    }
+
+    @Override
+    public boolean offerFirst(E e) {
+        if (e == null)
+            throw new NullPointerException();
         if (a == null)
             a = (E[]) new Object[j];
         if (n == a.length)
@@ -66,37 +73,82 @@ public class FastArrayDeque<E> extends AbstractCollection<E> {
         int i = i();
         if (i == 0)
             i = a.length;
-        a[--i] = item;
+        a[--i] = e;
         ++n;
+        return true;
     }
 
-    public void addLast(E item) {
+    @Override
+    public boolean offerLast(E e) {
+        if (e == null)
+            throw new NullPointerException();
         if (a == null)
             a = (E[]) new Object[j];
         if (n == a.length)
             resize(2 * a.length);
         if (j == a.length)
             j = 0;
-        a[j++] = item;
+        a[j++] = e;
         ++n;
+        return true;
     }
 
-    public E remove() {
-        return removeFirst();
-    }
-
+    @Override
     public E pop() {
         return removeFirst();
     }
 
+    @Override
+    public E remove() {
+        return removeFirst();
+    }
+
+    @Override
     public E removeFirst() {
-        E item = getFirst();
-        a[i()] = null;
+        E e = pollFirst();
+        if (e != null)
+            return e;
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public E removeLast() {
+        E e = pollLast();
+        if (e != null)
+            return e;
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object arg0) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object arg0) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public E poll() {
+        return pollFirst();
+    }
+
+    @Override
+    public E pollFirst() {
+        if (isEmpty())
+            return null;
+        int i = i();
+        E item = a[i];
+        a[i] = null;
         --n;
         return item;
     }
 
-    public E removeLast() {
+    @Override
+    public E pollLast() {
+        if (isEmpty())
+            return null;
         E item = getLast();
         a[--j] = null;
         --n;
@@ -105,23 +157,71 @@ public class FastArrayDeque<E> extends AbstractCollection<E> {
         return item;
     }
 
-    public E getFirst() {
-        if (isEmpty())
-            throw new NoSuchElementException();
+    @Override
+    public E element() {
+        return getFirst();
+    }
 
+    @Override
+    public E getFirst() {
+        E e = peekFirst();
+        if (e != null)
+            return e;
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public E getLast() {
+        E e = peekLast();
+        if (e != null)
+            return e;
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public E peek() {
+        return peekFirst();
+    }
+
+    @Override
+    public E peekFirst() {
+        if (isEmpty())
+            return null;
         return a[i()];
     }
 
-    public E getLast() {
+    @Override
+    public E peekLast() {
         if (isEmpty())
-            throw new NoSuchElementException();
-
+            return null;
         return a[j - 1];
+    }
+
+    @Override
+    public int size() {
+        return n;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    @Override
+    public void clear() {
+        n = 0;
+        if (a != null)
+            j = a.length;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new RingIterator();
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
